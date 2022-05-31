4b24d2d0f2aaea9c759938cafc399f1c51487037
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-05-31 12:49:29 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-
-
 import React from 'react';
-import { Provider } from 'react-redux';
 import axios from "axios";
-axios.defaults.baseURL=process.env.REACT_APP_API;
-
-import App from "../src/App.jsx";
 import LoginCard from "../src/components/LoginCard/LoginCard.jsx";
 import GlassesCalc from '../src/components/GlassesCalc/GlassesCalc.jsx';
+
+axios.defaults.baseURL=process.env.REACT_APP_API;
 
 export default function index()
 {
